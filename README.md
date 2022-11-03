5894b197311b89da2cd2d2d1ba44465deab59bd7
Cristian Amaya <v-cristian.amaya@shutterfly.com>
2022-11-03 16:32:27 -0500

    Small fixes and tweaks

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # rl_project_frozen_lake
+
+```bash
+.
+├── project/
+│   ├── agents/
+│   │   ├── core.py
+│   │   ├── dyna_q_agent.py
+│   │   └── q_learning_agent.py
+│   └── enviroment.py
+├── saves/
+│   ├── agent_values/
+│   ├── maps/
+│   └── results/
+├── outputs/
+├── explore_results.ipynb
+└── main.py
+```
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import gym
 import pickle
 import argparse
+import numpy as np
 from datetime import datetime
 from project.agents.core import BaseAgent
 from project.agents.q_learning_agent import QLearningAgent
@@ -15,7 +16,7 @@ parser.add_argument('--agent', type=str, default="QLearning")
 parser.add_argument('--render_steps', type=int, default=50)
 
 
-def save_results(agent_type, q_values, steps, n_episodes):
+def save_results(agent_type, q_values, steps,n_rewards,n_state_visits, n_episodes):
     result_folder = 'saves/results'
     if not os.path.exists(result_folder):
         os.mkdir(result_folder)
@@ -25,6 +26,8 @@ def save_results(agent_type, q_values, steps, n_episodes):
     results = {'agent_type': agent_type,
                'q_values': q_values,
                'steps': steps,
+               'n_rewards': n_rewards,
+               'n_state_visits': n_state_visits,
                'episodes': n_episodes}
     with open(results_path, 'wb') as handle:
         pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
@@ -37,15 +40,19 @@ def episode(agent: BaseAgent, env: gym.Env, render: bool = False):
         env.render()
     terminated = False
     steps = 0
+    rewards = 0
+    state_visits = np.zeros(48)
     while not terminated:
         steps += 1
         observation, reward, terminated, truncated, info = env.step(action)
+        rewards +=reward
+        state_visits[observation] +=1 
         if terminated or truncated:
             agent.agent_end(reward)
             observation, info = env.reset()
             break
         action = agent.agent_step(observation, reward)
-    return steps
+    return steps, rewards, state_visits
 
 
 def main():
@@ -60,6 +67,7 @@ def main():
     if agent_type == "QLearning":
         agent = QLearningAgent()
     else:
+        agent_type = "DynaQ"
         agent = DynaQAgent()
     agent_info = {"num_actions": 4,
                   "num_states": 16,
@@ -68,13 +76,17 @@ def main():
                   "discount": 1.0}
     agent.agent_init(agent_info)
     n_steps = []
+    n_rewards = []
+    n_state_visits = []
     for i in range(episodes):
         render_bool = i % render_steps
-        steps = episode(agent, map.env, render_bool)
+        steps, rewards, state_visits = episode(agent, map.env, render_bool)
         print("Episode {} finished after {} steps".format(i+1, steps+1))
         n_steps.append(steps)
+        n_rewards.append(rewards)
+        n_state_visits.append(state_visits)
     agent.agent_save()
-    save_results(agent_type, agent.q_values, n_steps, episodes)
+    save_results(agent_type, agent.q_values, n_steps,n_rewards,n_state_visits, episodes)
     map.env.close()
 
 
